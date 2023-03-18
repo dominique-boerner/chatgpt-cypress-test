f24bb1c5cfca4cb710ab07c8f8875cb085749a16
Dominique Börner <dominique@mukosoft.de>
2023-03-18 16:51:42 +0100

    Some clean-ups

--- example/script.js
+++ example/script.js
@@ -1,40 +1,39 @@
-const todoListEl = document.querySelector(".todo-list");
-const addTodoBtnEl = document.querySelector(".btn-add-todo");
-const todoInputEl = document.querySelector(".input-todo");
+const todoListEl = document.querySelector("#todo-list");
+const addTodoBtnEl = document.querySelector("#btn-add-todo");
+const todoInputEl = document.querySelector("#input-todo");
+const todoEmptyStateEl = document.querySelector("#todo-empty-state");
 
 const _state = {
-    todos: [],
-    newTodo: ""
-}
+  todos: [],
+  newTodo: "",
+};
 
 const handler = {
-    set(obj, prop, value) {
-        obj[prop] = value;
-        render();
-    },
+  set(obj, prop, value) {
+    obj[prop] = value;
+    render();
+  },
 };
 
 const state = new Proxy(_state, handler);
 
 // This will be called initially.
 (() => {
-    addTodoBtnEl.addEventListener("click", () => addTodo(todoInputEl.value));
+  addTodoBtnEl.addEventListener("click", () => addTodo(todoInputEl.value));
 })();
 
 const addTodo = (todo) => {
-    state.todos = [...state.todos, todo];
-    render();
-}
+  state.todos = [...state.todos, todo];
+  render();
+};
 
 const render = () => {
-    todoListEl.innerHTML = "";
-    state.todos.forEach(todo => {
-        const todoEl = document.createElement("li");
-        todoEl.textContent = todo;
-        todoListEl.appendChild(todoEl);
-    });
-    todoInputEl.value = "";
-}
-
-
-
+  todoListEl.innerHTML = "";
+  state.todos.forEach((todo) => {
+    const todoEl = document.createElement("li");
+    todoEl.textContent = todo;
+    todoListEl.appendChild(todoEl);
+  });
+  todoInputEl.value = "";
+  state.todos.length > 0 ? (todoEmptyStateEl.style.display = "none") : "block";
+};
